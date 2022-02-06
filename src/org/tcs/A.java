28417d9cce6b7c92137e98cfdaa8f1f9fbb3a5be
NaveenBiru <sendmailfornaveen@gmail.com>
2022-02-05 20:16:57 -0900

    complete

---
+++ src/org/tcs/A.java
@@ -0,0 +1,12 @@
+package org.tcs;
+
+public class A {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		System.out.println("total="+(a+b+c));
+		System.out.println("mul total="+(a*b*c));
+
+	}
+}
