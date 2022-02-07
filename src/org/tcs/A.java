d83cac8ddb22c0ce0efc8c6b20430389f6c4f2fd
NaveenBiru <sendmailfornaveen@gmail.com>
2022-02-06 19:14:46 -0900

    push to kumar

--- src/org/tcs/A.java
+++ src/org/tcs/A.java
@@ -7,6 +7,12 @@ public class A {
 		int c=30;
 		System.out.println("total="+(a+b+c));
 		System.out.println("mul total="+(a*b*c));
+		int d=50;
+		int e=80;
+		int f=30;
+		System.out.println("total="+(d+e+f));
+		System.out.println("mul total="+(e*d*f));
+		
 
 	}
 }
